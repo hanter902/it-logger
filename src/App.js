1c513ea7d2bda558b120dac3df32e9fedf552182
xuan <dangthanhxuan902@gmail.com>
2020-08-29 13:17:52 +0700

    init it-logger project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, Fragment } from 'react';
+import Searchbar from './components/layout/Searchbar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  useEffect(() => {
+    // Init Materialize Js
+   M.AutoInit(); 
+  })
+  return <Fragment>
+    <Searchbar />
+    <div className="container">
+      <AddBtn />
+      <AddLogModal />
+      <Logs />
     </div>
-  );
+  </Fragment>
 }
 
 export default App;
